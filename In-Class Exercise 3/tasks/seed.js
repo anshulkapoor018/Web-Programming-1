3324ca51442ad326171037c9fdcc1077407d025a
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-02-23 19:04:34 -0500

    zip

--- In-Class Exercise 3/tasks/seed.js
+++ In-Class Exercise 3/tasks/seed.js
@@ -11,13 +11,13 @@ const main = async () => {
     //Creating Second Band
     const Maroon5 = await bands.addBand("Maroon 5",["Adam Levine", "Jesse Carmichael", "Mickey Madden", "Matt Flynn", "Ryan Dusick"], 1994, ["Rock"], "NBC");
 
-    //Creating first Band
+    //Creating third Band
     const Coldplay = await bands.addBand("Colplay",["Chris Martin", "Guy Berryman", "Will Champion", "Jonny Buckland", "Phil Harvey"], 1996, ["Rock"], "Atlantic");
 
-    //Creating Second Band
+    //Creating fourth Band
     const Beatles = await bands.addBand("The Beatles",["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"], 1964, ["Rock", "Pop"], "Apple");
 
-    //Creating first Band
+    //Creating fifth Band
     const RollingStones = await bands.addBand("Rolling Stones",["Mick Jagger", "Keith Richards", "Ronnie Wood", "Charlie Watts"], 1962, ["Rock", "Blues", "Rock and Roll"], "Universal");
 
     console.log('Done seeding database');
